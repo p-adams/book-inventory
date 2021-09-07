8eb639c05cdb0664431cc10e2e29213c76d318bb
Paul Adams <ghusiyy@gmail.com>
2021-09-06 17:25:39 -0700

    render columns dynamically based on default args

--- src/main/kotlin/com/example/bookinventory/BookInventoryApplication.kt
+++ src/main/kotlin/com/example/bookinventory/BookInventoryApplication.kt
@@ -1,11 +1,7 @@
 package com.example.bookinventory
 
 import javafx.application.Application
-import javafx.geometry.Insets
 import javafx.scene.Scene
-import javafx.scene.control.TableColumn
-import javafx.scene.control.TableView
-import javafx.scene.layout.VBox
 import javafx.stage.Stage
 
 class BookInventoryApplication : Application() {
@@ -13,10 +9,8 @@ class BookInventoryApplication : Application() {
     override fun start(stage: Stage) {
         val bookInventory = BookInventoryController()
         stage.title = "Book Inventory"
-        val columns =  listOf<String>("")
-        val scene = Scene(createTable(
-            columns
-        ), 600.0, 500.0)
+
+        val scene = Scene(createTable(), 600.0, 500.0)
         stage.scene = scene
         stage.show()
     }
--- src/main/kotlin/com/example/bookinventory/Table.kt
+++ src/main/kotlin/com/example/bookinventory/Table.kt
@@ -5,26 +5,14 @@ import javafx.scene.control.TableColumn
 import javafx.scene.control.TableView
 import javafx.scene.layout.VBox
 
-fun createTable(columns: List<String>): VBox{
+fun createTable(columns: List<String> = listOf("Id", "Title", "Author", "Publisher", "Date", "Volumes", "Pages")): VBox{
     val table = TableView<Book>()
     // ensure that the extra space in table column header will be distributed among columns
     table.columnResizePolicy = TableView.CONSTRAINED_RESIZE_POLICY
     table.isEditable = true
-    // columns
-    val idCol = TableColumn<Book, String>("Id")
-    val titleCol = TableColumn<Book, String>("Title")
-    val authorCol = TableColumn<Book, String>("Author")
-    val publicationDateCol = TableColumn<Book, String>("Date")
-    val volumesCol = TableColumn<Book, String>("Volumes")
-    val pagesCol = TableColumn<Book, String>("Pages")
-    table.columns.addAll(
-        idCol,
-        titleCol,
-        authorCol,
-        publicationDateCol,
-        volumesCol,
-        pagesCol
-    )
+    for (column in columns.iterator()) {
+        table.columns.add(TableColumn<Book, String>(column))
+    }
     // set up view box
     val viewBox = VBox()
     viewBox.spacing = 5.0
