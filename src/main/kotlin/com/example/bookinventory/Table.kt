c370d48bddef2fe90e2157dc300628467450380b
Paul Adams <ghusiyy@gmail.com>
2021-09-07 23:16:37 -0700

    impl for Column data class

--- src/main/kotlin/com/example/bookinventory/Table.kt
+++ src/main/kotlin/com/example/bookinventory/Table.kt
@@ -5,13 +5,35 @@ import javafx.scene.control.TableColumn
 import javafx.scene.control.TableView
 import javafx.scene.layout.VBox
 
-fun createTable(columns: List<String> = listOf("Id", "Title", "Author", "Publisher", "Date", "Volumes", "Pages")): VBox{
+data class Column(
+    val label: String,
+    val value: String,
+    val nestedColumns: List<Column> = listOf(Column("", ""))
+)
+
+fun createTable(columns: List<Column> = listOf(
+    Column("id", "Id"),
+    Column("title", value = "Title"),
+    Column("author", "Author"),
+    Column("publisher","Publisher", nestedColumns = listOf(Column("name", "Name"), Column("date", "Date"))),
+    Column("date", "Date"),
+    Column("volumes", "Volumes"),
+    Column("pages", "Pages")
+)
+): VBox{
     val table = TableView<Book>()
     // ensure that the extra space in table column header will be distributed among columns
     table.columnResizePolicy = TableView.CONSTRAINED_RESIZE_POLICY
     table.isEditable = true
     for (column in columns.iterator()) {
-        table.columns.add(TableColumn<Book, String>(column))
+        // TODO: build nested columns for publisher column
+        val publisherCol = if(column.label == "publisher") {
+            val pubCol = TableColumn<Book, String>(column.value)
+        } else {
+            null
+        }
+
+        table.columns.add(TableColumn<Book, String>(column.label))
     }
     // set up view box
     val viewBox = VBox()
