2294ce5759a0e283bcd965fb52c5b1432c6af2e2
Paul Adams <ghusiyy@gmail.com>
2021-09-07 23:49:12 -0700

    impl for nested columns

--- src/main/kotlin/com/example/bookinventory/Table.kt
+++ src/main/kotlin/com/example/bookinventory/Table.kt
@@ -7,33 +7,41 @@ import javafx.scene.layout.VBox
 
 data class Column(
     val label: String,
-    val value: String,
+    val value: String
+)
+
+data class TableColumnData(
+    val column: Column,
     val nestedColumns: List<Column> = listOf(Column("", ""))
 )
 
-fun createTable(columns: List<Column> = listOf(
-    Column("id", "Id"),
-    Column("title", value = "Title"),
-    Column("author", "Author"),
-    Column("publisher","Publisher", nestedColumns = listOf(Column("name", "Name"), Column("date", "Date"))),
-    Column("date", "Date"),
-    Column("volumes", "Volumes"),
-    Column("pages", "Pages")
+fun createTable(columns: List<TableColumnData> = listOf(
+    TableColumnData(Column("id", "Id")),
+    TableColumnData(Column("title", value = "Title")),
+    TableColumnData(Column("author", "Author")),
+    TableColumnData(Column("publisher","Publisher"),
+        nestedColumns = listOf(Column("name", "Name"), Column("date", "Date"))
+    ),
+    TableColumnData(Column("volumes", "Volumes")),
+    TableColumnData(Column("pages", "Pages"))
 )
 ): VBox{
     val table = TableView<Book>()
     // ensure that the extra space in table column header will be distributed among columns
     table.columnResizePolicy = TableView.CONSTRAINED_RESIZE_POLICY
     table.isEditable = true
-    for (column in columns.iterator()) {
-        // TODO: build nested columns for publisher column
-        val publisherCol = if(column.label == "publisher") {
+    for (columnItem in columns.iterator()) {
+        val (column, nestedColumns) = columnItem
+        if(column.label == "publisher" && nestedColumns.isNotEmpty()) {
             val pubCol = TableColumn<Book, String>(column.value)
+            for(nestedColumn in nestedColumns) {
+                pubCol.columns.add(TableColumn<Book, String>(nestedColumn.value))
+            }
+            table.columns.add(pubCol)
         } else {
-            null
+            table.columns.add(TableColumn<Book, String>(column.value))
         }
 
-        table.columns.add(TableColumn<Book, String>(column.label))
     }
     // set up view box
     val viewBox = VBox()
